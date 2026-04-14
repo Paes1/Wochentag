e3cc89acbc24c01cc6f5bdc73f53d0a167e16bc0
user <user@user.com>
2026-04-14 09:53:24 +0200

    Implemented abstract methods isWeekday and isLaborday in Weekday

--- src/main/java/org/example/Weekday.java
+++ src/main/java/org/example/Weekday.java
@@ -1,11 +1,85 @@
 package org.example;
 
 public enum Weekday {
-  MONDAY,
-  TUESDAY,
-  WEDNESDAY,
-  THURSDAY,
-  FRIDAY,
-  SATURDAY,
-  SUNDAY
+  MONDAY {
+    @Override
+    public boolean isWeekday() {
+      return true;
+    }
+
+    @Override
+    public boolean isLaborday() {
+      return true;
+    }
+  },
+  TUESDAY {
+    @Override
+    public boolean isWeekday() {
+      return true;
+    }
+
+    @Override
+    public boolean isLaborday() {
+      return true;
+    }
+  },
+  WEDNESDAY {
+    @Override
+    public boolean isWeekday() {
+      return true;
+    }
+
+    @Override
+    public boolean isLaborday() {
+      return true;
+    }
+  },
+  THURSDAY {
+    @Override
+    public boolean isWeekday() {
+      return true;
+    }
+
+    @Override
+    public boolean isLaborday() {
+      return true;
+    }
+  },
+  FRIDAY {
+    @Override
+    public boolean isWeekday() {
+      return true;
+    }
+
+    @Override
+    public boolean isLaborday() {
+      return true;
+    }
+  },
+  SATURDAY {
+    @Override
+    public boolean isWeekday() {
+      return false;
+    }
+
+    @Override
+    public boolean isLaborday() {
+      return true;
+    }
+  },
+  SUNDAY {
+    @Override
+    public boolean isWeekday() {
+      return false;
+    }
+
+    @Override
+    public boolean isLaborday() {
+      return false;
+    }
+  };
+
+  public abstract boolean isWeekday();
+
+  public abstract boolean isLaborday();
 }
